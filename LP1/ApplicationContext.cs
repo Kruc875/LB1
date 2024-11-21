067ade7dc527f11beab9bf8a6ee4e73c817d2d98
Кристина <@DESKTOP-331NQFD>
2024-11-21 15:07:33 +0500

    Добавление миграции

--- LP1/ApplicationContext.cs
+++ LP1/ApplicationContext.cs
@@ -11,13 +11,13 @@ namespace LP1
     {
         public DbSet<User> Users { get; set; } = null!;
 
-        public ApplicationContext()
+        /*public ApplicationContext()
         {
             Database.EnsureCreated();
-        }
+        }*/
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb;Username=postgres;Password=14Kruc17" +
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=usersdb2;Username=postgres;Password=14Kruc17" +
                 "");
         }
     }
